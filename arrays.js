28503c195c3c9f61fdab6bcd526e0c5c1c00fdbe
Isaiah Richardson <capcinematic@gmail.com>
2022-11-09 21:02:42 -0700

    Completed

--- arrays.js
+++ arrays.js
@@ -1,5 +1,18 @@
 var deskTools = ["pen", "cup", "lighter", "mouse"]
+deskTools.push()
 
 var luckyNumbers = [3, 5, 7, 23]
+luckyNumbers.shift()
 
 var roomToggles = [true, false, true, false]
+roomToggles.pop()
+
+
+
+
+// Above I used array methods to alter varibles. 
+// The methods I used are Push, Shift and Pop.
+// Push will take a new element and add it to the end of an array.
+// Shift will remove the first element which in this cas would be position 0 valued at 3.
+// Pop will remove the last element of an array which will be the extra boolean "false".
+// I can see Push being very helpful for a growing list of names like a roster or reservation list.
